63bca598d4f4c4d51f01181ac005ffa8b9566786
Nemo <me@captnemo.in>
2018-01-06 13:10:55 +0530

    tf fmt

--- docker/locals.tf
+++ docker/locals.tf
@@ -1,12 +1,15 @@
 locals {
   traefik_common_labels {
-    "traefik.enable"                                   = "true"
+    "traefik.enable" = "true"
+
     // HSTS
-    "traefik.frontend.headers.SSLTemporaryRedirect"    = "true"
-    "traefik.frontend.headers.STSSeconds"              = "2592000"
-    "traefik.frontend.headers.STSIncludeSubdomains"    = "false"
+    "traefik.frontend.headers.SSLTemporaryRedirect" = "true"
+    "traefik.frontend.headers.STSSeconds"           = "2592000"
+    "traefik.frontend.headers.STSIncludeSubdomains" = "false"
+
     // X-Powered-By, Server headers
-    "traefik.frontend.headers.customResponseHeaders"   = "${var.xpoweredby}"
+    "traefik.frontend.headers.customResponseHeaders" = "${var.xpoweredby}"
+
     // X-Frame-Options
     "traefik.frontend.headers.customFrameOptionsValue" = "${var.xfo_allow}"
     "traefik.frontend.headers.contentTypeNosniff"      = "true"
