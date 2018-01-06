3fc9b585f1992e51cf10867c67501c3d6eed45cb
Nemo <me@captnemo.in>
2018-01-06 13:09:21 +0530

    minor comments

--- docker/locals.tf
+++ docker/locals.tf
@@ -1,10 +1,13 @@
 locals {
   traefik_common_labels {
     "traefik.enable"                                   = "true"
+    // HSTS
     "traefik.frontend.headers.SSLTemporaryRedirect"    = "true"
     "traefik.frontend.headers.STSSeconds"              = "2592000"
     "traefik.frontend.headers.STSIncludeSubdomains"    = "false"
+    // X-Powered-By, Server headers
     "traefik.frontend.headers.customResponseHeaders"   = "${var.xpoweredby}"
+    // X-Frame-Options
     "traefik.frontend.headers.customFrameOptionsValue" = "${var.xfo_allow}"
     "traefik.frontend.headers.contentTypeNosniff"      = "true"
     "traefik.frontend.headers.browserXSSFilter"        = "true"
