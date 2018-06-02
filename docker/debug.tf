ee54248ae055390d2e029e7aae6239459391c4f0
Nemo <me@captnemo.in>
2018-06-03 00:32:31 +0530

    Re-enable headerdebug

--- docker/debug.tf
+++ docker/debug.tf
@@ -1,16 +1,23 @@
-# resource "docker_container" "headerdebug" {
-#   name  = "headerdebug"
-#   image = "${docker_image.headerdebug.latest}"
-#   restart               = "unless-stopped"
-#   destroy_grace_seconds = 30
-#   must_run              = true
-#   memory = 16
-#   labels = "${merge(
-#     local.traefik_common_labels,
-#     map(
-#       "traefik.frontend.rule", "Host:debug.in.${var.domain},debug.${var.domain}",
-#       "traefik.port", 8080,
-#       "traefik.enable", "true",
-#     ))}"
-# }
+resource "docker_container" "headerdebug" {
+  name                  = "headerdebug"
+  image                 = "${docker_image.headerdebug.latest}"
+  restart               = "unless-stopped"
+  destroy_grace_seconds = 30
+  must_run              = true
+  memory                = 16
 
+  labels = "${merge(
+    local.traefik_common_labels,
+    map(
+      "traefik.frontend.rule", "Host:debug.in.${var.domain},debug.${var.domain}",
+      "traefik.port", 8080,
+      "traefik.enable", "true",
+    ))}"
+}
+
+# Helps debug traefik reverse proxy headers
+# Highly recommended!
+resource "docker_image" "headerdebug" {
+  name          = "${data.docker_registry_image.headerdebug.name}"
+  pull_triggers = ["${data.docker_registry_image.headerdebug.sha256_digest}"]
+}
--- docker/images.tf
+++ docker/images.tf
@@ -13,13 +13,6 @@ resource "docker_image" "ubooquity" {
   pull_triggers = ["${data.docker_registry_image.ubooquity.sha256_digest}"]
 }
 
-# Helps debug traefik reverse proxy headers
-# Highly recommended!
-# resource "docker_image" "headerdebug" {
-#   name          = "${data.docker_registry_image.headerdebug.name}"
-#   pull_triggers = ["${data.docker_registry_image.headerdebug.sha256_digest}"]
-# }
-
 resource "docker_image" "lychee" {
   name          = "${data.docker_registry_image.lychee.name}"
   pull_triggers = ["${data.docker_registry_image.lychee.sha256_digest}"]
