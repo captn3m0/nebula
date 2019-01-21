fa99d0de64505519144d82ae2779b9d84668199d
Nemo <me@captnemo.in>
2019-01-22 00:44:25 +0530

    Switch to gitea master

--- gitea/data.tf
+++ gitea/data.tf
@@ -1,9 +1,7 @@
 # https://github.com/go-gitea/gitea/releases
 data "docker_registry_image" "gitea" {
+  # not bleeding, this is hemorrhaging edge
   name = "gitea/gitea:latest"
-
-  # Currently on latest master to avoid https://github.com/go-gitea/gitea/issues/5704
-  # name = "gitea/gitea:1.7"
 }
 
 data "docker_registry_image" "redis" {
