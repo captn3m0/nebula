ff3b56231bb6ea68a8814fcfae16545b3ea32a1a
Nemo <me@captnemo.in>
2019-03-26 12:48:43 +0530

    fix rss-bridge

--- rss-bridge.tf
+++ rss-bridge.tf
@@ -2,14 +2,21 @@ module "rss-bridge" {
   name   = "rss-bridge"
   source = "modules/container"
 
-  image = "rss-bridge/rss-bridge:latest"
+  image = "rssbridge/rss-bridge:latest"
 
   web {
     expose = "true"
     host   = "rss-bridge.${var.root-domain}"
   }
 
-  networks = ["${data.docker_network.bridge.id}"]
+  networks_advanced = [
+    {
+      name = "bridge"
+    },
+    {
+      name = "traefik"
+    },
+  ]
 
   volumes = [{
     container_path = "/app/public/whitelist.txt"
