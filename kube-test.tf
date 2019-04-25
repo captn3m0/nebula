07d44ca39aa80c3e3ae5b02d5f9815b2a38c4224
Nemo <me@captnemo.in>
2019-04-26 03:16:25 +0530

    Comment out unused kubernetes stuff

--- kube-test.tf
+++ kube-test.tf
@@ -1,22 +1,20 @@
 // Bring up a simple test container
 // In the controller node
+# resource "kubernetes_pod" "nginx" {
+#   metadata {
+#     name      = "terraform-example"
+#     namespace = "default"
+#   }
+#   spec {
+#     toleration {
+#       key      = "node-role.kubernetes.io/master"
+#       operator = "Exists"
+#       effect   = "NoSchedule"
+#     }
+#     container {
+#       image = "nginx:latest"
+#       name  = "nginx"
+#     }
+#   }
+# }
 
-resource "kubernetes_pod" "nginx" {
-  metadata {
-    name      = "terraform-example"
-    namespace = "default"
-  }
-
-  spec {
-    toleration {
-      key      = "node-role.kubernetes.io/master"
-      operator = "Exists"
-      effect   = "NoSchedule"
-    }
-
-    container {
-      image = "nginx:latest"
-      name  = "nginx"
-    }
-  }
-}
