37bb59eda355e819dc9fe143a5019b9139c46b13
Nemo <me@captnemo.in>
2019-04-26 03:16:37 +0530

    Run a jupyter notebook

---
+++ jupyter.tf
@@ -0,0 +1,13 @@
+module "jupyter" {
+  name   = "jupyter"
+  source = "modules/container"
+  image  = "jupyter/tensorflow-notebook"
+
+  ports = [
+    {
+      internal = 8888
+      external = 1112
+      ip       = "${var.ips["tun0"]}"
+    },
+  ]
+}
