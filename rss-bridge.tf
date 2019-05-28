bd27db6aa66d1646ae6f075ad4126831655e115a
Nemo <me@captnemo.in>
2019-05-28 16:05:16 +0530

    Switch to official PHP base image

--- rss-bridge.tf
+++ rss-bridge.tf
@@ -2,7 +2,7 @@ module "rss-bridge" {
   name   = "rss-bridge"
   source = "modules/container"
 
-  image = "rssbridge/rss-bridge:latest"
+  image = "captn3m0/rss-bridge:docker-improvements"
 
   web {
     expose = "true"
