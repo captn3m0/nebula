373793fcb510633809c9382b7b6592530fde2f00
Nemo <commits@captnemo.in>
2020-10-11 18:29:56 +0530

    Setup one for audiobooks

--- media/gonic.tf
+++ media/gonic.tf
@@ -34,3 +34,41 @@ module "gonic" {
     }
   ]
 }
+
+// Run a second instance, just for Audiobooks
+module "gonic-audio" {
+  source = "../modules/container"
+  image  = "sentriz/gonic"
+  name   = "gonic-audio"
+
+  resource {
+    memory      = "256"
+    memory_swap = "256"
+  }
+
+  web {
+    port   = 80
+    host   = "audiobooks.bb8.fun"
+    expose = true
+  }
+
+  env = [
+    "GONIC_SCAN_INTERVAL=60",
+    "GONIC_MUSIC_PATH=/books",
+    "GONIC_DB_PATH=/data/gonic-audio.db"
+  ]
+
+  networks = "${list(docker_network.media.id, data.docker_network.bridge.id)}"
+
+  volumes = [
+    {
+      host_path      = "/mnt/xwing/config/gonic"
+      container_path = "/data"
+    },
+    {
+      host_path      = "/mnt/xwing/media/Music/Audiobooks"
+      container_path = "/books"
+      read_only      = true
+    }
+  ]
+}
