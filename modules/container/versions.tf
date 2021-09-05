afc75b2e5504000db93ca12b66cdc73d41fd9a0b
Nemo <commits@captnemo.in>
2021-09-05 13:18:49 +0530

    tf upgrade

--- modules/container/versions.tf
+++ modules/container/versions.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_version = ">= 1.0.6"
+}
 
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
 }
