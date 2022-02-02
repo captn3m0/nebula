51dd19cdc8e965598d94839dcd83f66c0b24055b
Nemo <commits@captnemo.in>
2022-02-02 19:06:13 +0530

    add prowlarr

---
+++ media/prowlarr.tf
@@ -0,0 +1,33 @@
+module "prowlarr" {
+  name   = "prowlarr"
+  source = "../modules/container"
+  image  = "linuxserver/prowlarr:nightly"
+
+  web = {
+    expose = true
+    port   = 9696
+    host   = "prowlarr.${var.domain}"
+    auth   = true
+  }
+
+  resource = {
+    memory      = 512
+    memory_swap = 1024
+  }
+
+  volumes = [
+    {
+      host_path      = "/mnt/xwing/config/prowlarr"
+      container_path = "/config"
+    }
+  ]
+
+  env = [
+    "PUID=1004",
+    "PGID=1003",
+    "TZ=Asia/Kolkata",
+  ]
+
+  networks = [docker_network.media.id, data.docker_network.bridge.id]
+}
+
