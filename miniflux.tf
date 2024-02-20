81d77af10241843bb58b0422c9b8415f284991dc
Nemo <commits@captnemo.in>
2024-02-20 23:45:58 +0530

    miniflux upgrade

--- miniflux.tf
+++ miniflux.tf
@@ -1,7 +1,7 @@
 module "miniflux-container" {
   name   = "miniflux"
   source = "./modules/container"
-  image  = "miniflux/miniflux:2.0.51"
+  image  = "miniflux/miniflux:2.1.0"
 
   web = {
     expose = true
